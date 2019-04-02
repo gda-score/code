b30882c212fb4985e63c6086672671474bec139f
Mohamed Ibrahim <m3eeza@yahoo.com>
2019-04-02 13:36:11 +0200

    packaging

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gda-score-code",
+    version="1.1.0",
+    author="Paul Francis",
+    description="Tools for generating General Data Anonymity Scores (www.gda-score.org)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gda-score/code",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
