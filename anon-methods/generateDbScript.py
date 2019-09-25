c434e4de7e5ed8954f6ccd6d3c74eaa6304b5128
Paul Francis <paul@francis.com>
2019-09-25 17:29:01 +0200

    moved to utility and anonymization methods repos

--- anon-methods/generateDbScript.py
+++
@@ -1,8 +0,0 @@
-import sys
-from common.gdaTool import gdaTool
-import pprint
-pp = pprint.PrettyPrinter(indent=4)
-
-gdaToolObj = gdaTool()
-pp.pprint(gdaToolObj)
-gdaToolObj.generateDBSqlForTable(sys.argv, 'rawDb')
--- anon-methods/sql-differential-privacy/src/main/scala/com/uber/engsec/dp/schema/CachingSchema.scala
+++
@@ -1,7 +0,0 @@
-package org.apache.calcite.jdbc
-
-import org.apache.calcite.schema.{Schema, SchemaPlus}
-
-object SchemaAdapter {
-  def toRootSchemaPlus(schema: Schema, name: String): SchemaPlus = new CachingCalciteSchema(null, schema, name).plus()
-}
