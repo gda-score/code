f67e59fa83223ed42b97edf93b1c8026765c4510
Paul Francis <paul@francis.com>
2020-11-03 09:17:23 +0100

    update version number in setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setuptools.setup(
     #name="gda-score-code",
     name="gda-score-code-test",
     # version="2.5.0",
-    version="2.5.9",  # for test.pypi
+    version="2.5.10",  # for test.pypi
     author="Paul Francis",
     description="Tools for generating General Data Anonymity Scores (www.gda-score.org)",
     long_description=long_description,
